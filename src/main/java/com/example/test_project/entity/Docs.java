32cbde54fcb6dddd0f2fa6ff9aea906d2d378231
bender <bender7716@gmail.com>
2024-05-27 22:24:06 +0300

    Добавлен слой entity и точка входа в приложение

---
+++ src/main/java/com/example/test_project/entity/Docs.java
@@ -0,0 +1,31 @@
+package com.example.test_project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "docs")
+public class Docs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "date")
+    private String date;
+}
